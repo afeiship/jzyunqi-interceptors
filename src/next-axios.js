bb4147a3f4534683575f5eba3155f7b8921c77ef
afeiship <1290657123@qq.com>
2017-04-01 14:27:47 +0800

    add finalily shim

--- src/next-axios.js
+++ src/next-axios.js
@@ -4,6 +4,10 @@
   var axios = global.axios || require('axios');
   var Q = global.Q || require('q');
 
+  //shim require.finally:
+  if (typeof module !== 'undefined' && module.exports) {
+    require('promise.prototype.finally').shim();
+  }
 
   var Axios = nx.declare('nx.Axios', {
     methods: {
